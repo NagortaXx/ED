7e3f0a24a9f942e9caa1055b717a5e38075b3b38
alumno <alumno@pc35>
2024-05-10 11:53:57 +0200

    Mensaje

---
+++ EjemploGit1/src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+public double add(double number1, double number2){
+return number1 + number2;
+}
+public double subtract(double number1, double number2){
+return number1 - number2;
+}
+public double multiply(double number1, double number2) {
+return number1 * number2;
+}
+public double divide(double number1, double number2){
+return number1 / number2;
+}
+public boolean isGreaterThan(double number1, double number2) {
+    return number1 > number2;
+}
+public boolean isGreaterThan2(double number1, double number2) {
+    return number1 < number2;
+}
+
+}
